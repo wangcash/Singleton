d0f620112b05909d93cc35b75ca3cfa55a7760e2
wangcash <wangcash@gmail.com>
2013-03-22 04:59:09 +0800

    Initial Commit

---
+++ Singleton/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Singleton
+//
+//  Created by Cash on 13-3-22.
+//  Copyright (c) 2013年 imwangwei.cn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
